911a43a3d98a98c0fbbb80cc276c4cee252ae24d
Roman Juranek <RomanJuranek@users.noreply.github.com>
2020-09-17 21:40:09 +0200

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(current_folder, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='diamond_space',
-    version='1.0.2',
+    version='0.0.1',
     description='Diamond space transform for python',
     long_description=long_description,
     long_description_content_type='text/markdown',
